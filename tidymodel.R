55f13d7a6562240d5f152506894b8447f0a672a3
Isioma Mabatah <Mabatah93@gmail.com>
2021-03-31 21:37:03 -0500

    commit

--- tidymodel.R
+++ tidymodel.R
@@ -1,8 +1,7 @@
 library(dplyr)
 library(ggplot2)
 theme_set(theme_minimal())
-library(rsample)
-library(recipes)
+library(tidymodels)
 
 # Data
 data(ames, package = "modeldata")
@@ -28,7 +27,7 @@ ames_train <- training(ames_split_str)
 ames_test <- testing(ames_split_str)
 
 # Feature Engineering
-simple_ames <- 
+ames_recipe <- 
   recipe(Sale_Price_Log10 ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type,
          data = ames_train) %>% 
   step_log(Gr_Liv_Area, base = 10) %>%
@@ -37,5 +36,40 @@ simple_ames <-
   step_interact( ~ Gr_Liv_Area:starts_with("Bldg_Type_") ) %>% 
   step_ns(Latitude, Longitude, deg_free = 20)
 
-simple_ames <- prep(simple_ames, training = ames_train)
+simple_ames <- prep(ames_recipe, training = ames_train)
+
+# Fit
+linear_reg() %>% 
+  set_engine("lm") %>%  translate()
+
+lm_mod <- 
+  linear_reg() %>% 
+  set_engine("lm")
+
+lm_form_fit <- 
+  lm_mod %>% 
+  fit(Sale_Price_Log10 ~ Latitude + Longitude, data = ames_train)
+
+lm_form_fit %>%
+  pluck("fit") %>% 
+  summary()
+
+lm_form_fit %>% tidy()
+
+ames_test %>% 
+  select(Sale_Price_Log10) %>% 
+  bind_cols(predict(lm_form_fit, ames_test)) %>% 
+  bind_cols(predict(lm_form_fit, ames_test, type = "pred_int"))
+
+# Workflow
+lm_wflow <- workflow() %>% 
+  add_model(lm_mod) %>% 
+  add_formula(Sale_Price_Log10 ~ Latitude + Longitude)
+
+# - fit
+lm_fit <- fit(lm_wflow, data = ames_train)
+
+lm_wflow %>% 
+  remove_formula() %>% 
+  add_recipe(simple_ames)
 
