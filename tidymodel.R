fec764cf0b9f80c6c0845ebb71d9642fc443df58
Isioma Mabatah <Mabatah93@gmail.com>
2021-04-13 17:46:31 -0500

    Logistic Regression

--- tidymodel.R
+++ tidymodel.R
@@ -3,10 +3,13 @@ library(dplyr)
 library(tidymodels)
 theme_set(theme_minimal())
 
-# Data ----
+# DATA ----
 home_sales <- readRDS(file = "Data/home_sales.rds") %>% as_tibble()
-
-# Data Resampling: (rsamples) ----
+telecom <- readRDS("Data/telecom_df.rds") %>% as_tibble()
+leads <- readRDS("Data/leads_df.rds") %>% as_tibble()
+#
+# RESAMPLING: (rsamples) ----
+# Home Sales ====
 # - split
 home_split <- initial_split(home_sales, 
                             prop = 0.7,
@@ -14,12 +17,19 @@ home_split <- initial_split(home_sales,
 home_train <- home_split %>% training()
 home_test <- home_split %>% testing()
 
+# Telecom ====
+# - split
+telecom_split <- initial_split(telecom,
+                               prop = 0.75,
+                               strata = canceled_service)
+
+telecom_train <- telecom_split %>% training()
+telecom_test <- telecom_split %>% testing()
 
 #
-# Feature Engineering ----
-# Model Fitting: (parsnip) ----
-
-# Linear Regression
+# FEATURE ENGINEERING ----
+# MODEL FITTING: (parsnip) ----
+# Linear Regression: Home Sales ====
 # - fit
 lm_mod <- 
   linear_reg() %>% 
@@ -34,18 +44,36 @@ lm_fit <-
 lm_final <- 
   lm_mod %>% 
   last_fit(selling_price ~ ., split = home_split)
+#
+# Logistic Regression: Telecom ====
+# - fit
+log_model <- 
+  logistic_reg() %>% 
+  set_engine("glm") %>% 
+  set_mode("classification")
+
+log_fit <- 
+  log_model %>% 
+  fit(canceled_service ~ avg_call_mins + avg_intl_mins + monthly_charges,
+      data = telecom_train)
+log_fit %>% tidy()
+# - final
+log_final <- 
+  log_model %>% 
+  last_fit(canceled_service ~ avg_call_mins + avg_intl_mins + monthly_charges + months_with_company,
+           split = telecom_split)
+#
+# MODEL TUNING ----
+# MODEL EVALUATION: (yardstick) ----
+# Linear Regression: Home Sales ====
 # - prediction
-lm_test_results <- home_test %>% 
+home_sales_results <- home_test %>% 
   select(selling_price, home_age, sqft_living) %>% 
   mutate(lm_pred = lm_fit %>% predict(home_test) %>% pull)
-#
-# Model Tuning ----
-# Model Evaluation: (yardstick) ----
-# Linear Regression
-lm_test_results
+
 # - evaluation
-lm_test_results %>% rmse(truth = selling_price, estimate = lm_pred)
-lm_test_results %>% rsq(truth = selling_price, estimate = lm_pred)
+home_sales_results %>% rmse(truth = selling_price, estimate = lm_pred)
+home_sales_results %>% rsq(truth = selling_price, estimate = lm_pred)
 # - visual
 lm_test_results %>% 
   ggplot(aes(selling_price, lm_pred)) +
@@ -57,7 +85,8 @@ lm_test_results %>%
     y = "Predicted"
   ) +
   theme_bw()
-# - final evaluation
+
+# - FINAL evaluation
 lm_final %>% 
   collect_predictions() %>% 
   ggplot(aes(selling_price, .pred)) +
@@ -69,3 +98,42 @@ lm_final %>%
     y = "Predicted"
   ) +
   theme_bw()
+
+
+
+
+# Logistic Regression: Telecom ====
+# - prediction
+telecom_results <- telecom_test %>% 
+  select(canceled_service) %>% 
+  mutate(log_pred = log_fit %>% predict(telecom_test, type = "class") %>% pull,
+         log_prob = log_fit %>% predict(telecom_test, type = "prob") %>% pull(.pred_yes))
+# - confusion matrix
+telecom_results %>% conf_mat(truth = canceled_service, estimate = log_pred) %>% summary()
+telecom_results %>% conf_mat(truth = canceled_service, estimate = log_pred) %>% autoplot(type = "heatmap")
+telecom_results %>% conf_mat(truth = canceled_service, estimate = log_pred) %>% autoplot(type = "mosaic")
+
+telecom_results %>% accuracy(truth = canceled_service, estimate = log_pred)
+telecom_results %>% sens(truth = canceled_service, estimate = log_pred)
+telecom_results %>% spec(truth = canceled_service, estimate = log_pred)
+
+telecom_metrics <- metric_set(accuracy, sens, spec, roc_auc)
+telecom_results %>% telecom_metrics(truth = canceled_service, 
+                                    estimate = log_pred, log_prob)
+# - roc curve
+telecom_results %>% 
+  roc_curve(truth = canceled_service, estimate = log_prob) %>% 
+  autoplot()
+telecom_results %>% roc_auc(truth = canceled_service, estimate = log_prob)
+
+# - FINAL evaluation
+telecom_final_results <- log_final %>% collect_predictions()
+log_final %>% collect_metrics()
+
+telecom_final_metrics <- metric_set(accuracy, sens, spec, roc_auc)
+telecom_final_results %>% telecom_final_metrics(truth = canceled_service, 
+                                                estimate = .pred_class, .pred_yes)
+
+
+
+
